626d7e17e4a29db0cbe20ccfca85686626d8e312
Kevin Leyssens <kevinleyssens@hotmail.com>
2017-12-11 13:18:00 +0100

    Some comments

--- contracts/SimpleContract.sol
+++ contracts/SimpleContract.sol
@@ -1,32 +1,34 @@
 pragma solidity 0.4.18;
 
-
+// Simple contract written in solidity
 contract SimpleContract {
     address public owner;
     string public nameOwner;
 
+    // event, fired when the fallback function is called
     event Fallback(address sender, uint256 amount);
-    /**
-    * @dev Throws if called by any account other than the owner.
-    */
+
+    
+    // Throws if called by any account other than the owner.
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
-    // this is the constructor
+    // This is the constructor which has a name parameter
     function SimpleContract(string _nameOwner) public{
         owner = msg.sender;
         nameOwner = _nameOwner;
     }
 
-    // this is the fallback function. The payable parameter specifies that 
+    // This is the fallback function. The payable parameter specifies that 
     // you can deposit some ETH on the contract.
     function() payable public {
         // send event
         Fallback(msg.sender, msg.value);
     }
 
+    // This function can only called upon by the owner of this contract.x
     function withdrawMoney() onlyOwner public {
         owner.transfer(this.balance);
     }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ const SimpleContract = artifacts.require('SimpleContract');
 
 
 module.exports = function(deployer) {
-  // deploy SimpleContract
+  // deploy SimpleContract with an owners name as parameter
   deployer.deploy(SimpleContract,"Kevin Leyssens");
 }
